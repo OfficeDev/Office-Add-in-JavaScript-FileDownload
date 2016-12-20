3ad6df6318a9b96b85e631a140ceef3c8fa915ff
Reeza Ali <reezaali@hotmail.com>
2016-12-19 18:04:57 -0800

    Initial check-in

---
+++ FileDownloadSampleWeb/Home.js
@@ -0,0 +1,120 @@
+﻿/* 
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+
+(function () {
+    "use strict";
+
+    // The initialize function must be defined in each page of your add-in.
+    Office.initialize = function (reason) {
+
+        // A function must be assigned to Office.initialize. You can also include initialization code here. In
+        // this code sample, we don't have anything to initialize. 
+
+    };
+
+    $(document).ready(function () {
+        // Initialize the UI 
+        $('#button-text1').text("Download file");
+        $('#button-desc1').text("Download file");
+        $('#btnDownload1').click(
+           downloadFile);
+
+        $('#button-text2').text("Insert download link");
+        $('#button-desc2').text("Insert download link");
+        $('#btnDownload2').click(
+           InsertURL);
+    });
+
+    var downloadUri = location.origin + '/testdata.pdf'; // PDF file to download from the remote server.
+   
+    /*
+        For all Office clients and browsers, except Safari, the user gets prompted to save the file.
+        In Safari, the file is opened in another tab and the user manually chooses where to save the file.
+    */
+    function downloadFile() {
+        // The following line is taken from http://stackoverflow.com/questions/7944460/detect-safari-browser
+        if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {                        
+            insertAndClickLink();          
+        }
+        else {
+            // First download the file, then prompt the user to save it. 
+            // This function uses the file-saver.js library.
+            getFile(downloadUri)  
+            .then(function (file) {
+                saveAs(file, "myfile.pdf"); 
+            })
+            .catch(function (e) {
+                console.error(e);
+            });
+        }
+    }
+
+    /*
+        Downloads the file from the remote server.
+    */
+    function getFile(url) {
+        return new OfficeExtension.Promise(function (resolve, reject) {
+            // Use XHR to fetch the file from the remote server.
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.responseType = 'blob';
+
+            xhr.onload = function () {
+                try {
+                    if (xhr.status === 200) {
+                        resolve(xhr.response);
+                    }
+                    else if (xhr.status !== 200) {
+                        reject({
+                            error: 'Request failed. ' + xhr.response
+                        });
+                    }
+                } catch (e) {
+                    reject({
+                        error: e
+                    });
+                }
+            };
+            xhr.send();
+        });
+    }
+
+    /*
+        The following solution programmatically inserts a link and clicks it. 
+        Use with Safari browsers.
+    */
+    function insertAndClickLink() {
+
+        //Taken from http://stackoverflow.com/questions/3077242/force-download-a-pdf-link-using-javascript-ajax-jquery/29266135#29266135
+        try {
+            if (!window.ActiveXObject) {
+                var save = document.createElement('a');
+                save.href = downloadUri;
+                save.target = '_blank';                
+                save.download = "myfile.pdf" || 'unknown';
+
+                var evt = new MouseEvent('click', {
+                    'view': window,
+                    'bubbles': true,
+                    'cancelable': false
+                });
+
+                save.dispatchEvent(evt);
+                (window.URL || window.webkitURL).revokeObjectURL(save.href);
+            }
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    /*
+       This function inserts a URL into a DIV tag, which the user then clicks. This provides a good fallback experience. 
+   */
+    function InsertURL() {        
+        $("#InsertURL").append("<a href=\"" + downloadUri + "\" target=\"_blank\">PDF</a>");
+    }
+})();
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Office-Add-in-JavaScript-FileDownload
+# Download files in a Word Web Add-in
+
+This code sample teaches you the different techniques for downloading files programmatically in an Office Add-in. Different Office hosts and browsers (when using Office Online) may handle files differently. If you'd like to provide a uniform file download experience, use the techniques demonstrated in this code sample. This code sample also shows you how you can provide a fallback mechanism if the Office host or browser does not support one of the other methods. 
+
+## Table of Contents
+* [Change History](#change-history)
+* [Prerequisites](#prerequisites)
+* [Design templates used in this add-in](#design-templates-used-in-this-add-in)
+* [Get the FileSaver library](#get-the-filesaver-library)
+* [Run the project](#run-the-project)
+* [Questions and comments](#questions-and-comments)
+* [Additional resources](#additional-resources)
+
+## Change History
+
+November 3, 2016:
+
+* Initial version.
+
+## Prerequisites
+
+* Word 2016 for Windows (build 16.0.6727.1000 or later), Word Online, or Word for Mac (build 15.26 or later).
+* Visual Studio 2015 
+
+## Design templates used in this add-in
+
+- Landing page
+
+For more information about the design patterns, see [UX design pattern templates for Office Add-ins](https://dev.office.com/docs/add-ins/design/ux-design-patterns). And for sample implementations, see [Office-Add-in-UX-Design-Patterns-Code](https://github.com/OfficeDev/Office-Add-in-UX-Design-Patterns-Code).
+
+## Get the FileSaver library 
+
+This code sample uses the FileSaver.js library. Take the following steps after you have cloned this repo. 
+
+1. Clone the [FileSaver.js](https://github.com/eligrey/FileSaver.js/) repo.
+2. Copy the **FileSaver.min.js** file to the **Scripts** folder of this code sample.
+3. In Home.html, add a script tag to reference the FileSaver.min.js file.
+ 
+
+## Run the project
+
+1. Open the Visual Studio solution file. 
+2. Press **F5**. 
+3. When Word opens, click the **File download** button on the right end of the **Home** ribbon. The add-in opens in a task pane.
+4. Choose the **Download file** or **Insert download link** buttons.
+
+## Questions and comments
+
+We'd love to get your feedback about this sample. You can send your feedback to us in the *Issues* section of this repository.
+
+Questions about Microsoft Office 365 development in general should be posted to [Stack Overflow](http://stackoverflow.com/questions/tagged/office-js+API). If your question is about the Office JavaScript APIs, make sure that your questions are tagged with [office-js] and [API].
+
+## Additional resources
+
+* [Office add-in documentation](https://dev.office.com/docs/add-ins/overview/office-add-ins)
+* [Office Dev Center](http://dev.office.com/)
+* More Office Add-in samples at [OfficeDev on Github](https://github.com/officedev)
+
+## Copyright
+Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+
